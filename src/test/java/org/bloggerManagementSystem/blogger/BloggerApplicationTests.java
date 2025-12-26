50596a3e4cfbf561f2a5b599ab90d0339b3e32f4
pranalipolekar <pranalipolekar@gmail.com>
2025-12-26 18:32:16 +0530

    Add project files

---
+++ src/test/java/org/bloggerManagementSystem/blogger/BloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bloggerManagementSystem.blogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
